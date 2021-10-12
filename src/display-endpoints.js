6fafdc7b257c0e819857f1a0e148d981bc757569
Jonathan Keslin <github@jonathankeslin.com>
2021-10-12 15:32:12 -0700

    [fix] Bind displayEndpoint properly

--- src/display-endpoints.js
+++ src/display-endpoints.js
@@ -10,44 +10,44 @@ module.exports = {
     const allFunctions = this.serverless.service.getAllFunctions();
 
     for (const functionName of allFunctions) {
-      await this.displayEndpoint(functionName);
+      await displayEndpoint.call(this, functionName);
     }
 
     return allFunctions;
-  },
-
-  async displayEndpoint(functionName) {
-    const functionObject = this.serverless.service.getFunction(functionName);
-    const deployedName = functionObject.name;
-
-    const result = await this.provider.request('Lambda',
-      'getFunction', {
-        FunctionName: deployedName
-      });
-    const stack = await this.provider.request('CloudFormation',
-      'describeStacks', {
-        StackName: result.Tags['aws:cloudformation:stack-name']
-      });
-    const outputs = stack.Stacks[0].Outputs;
-    const serviceEndpointOutputRegex = this.provider.naming
-      .getServiceEndpointRegex();
-    outputs.filter(x => x.OutputKey.match(serviceEndpointOutputRegex))
-      .forEach((x) => {
-        const endpoint = x.OutputValue;
-        functionObject.events.forEach((event) => {
-          if (event.http) {
-            let method;
-            let path;
-            if (typeof event.http === 'object') {
-              method = event.http.method.toUpperCase();
-              path = event.http.path;
-            } else {
-              [method, path] = event.http.split(' ');
-              method = method.toUpperCase();
-            }
-            this.serverless.cli.consoleLog(`  ${method} - ${endpoint}/${path}`);
-          }
-        });
-      });
   }
 };
+
+async function displayEndpoint(functionName) {
+  const functionObject = this.serverless.service.getFunction(functionName);
+  const deployedName = functionObject.name;
+
+  const result = await this.provider.request('Lambda',
+    'getFunction', {
+      FunctionName: deployedName
+    });
+  const stack = await this.provider.request('CloudFormation',
+    'describeStacks', {
+      StackName: result.Tags['aws:cloudformation:stack-name']
+    });
+  const outputs = stack.Stacks[0].Outputs;
+  const serviceEndpointOutputRegex = this.provider.naming
+    .getServiceEndpointRegex();
+  outputs.filter(x => x.OutputKey.match(serviceEndpointOutputRegex))
+    .forEach((x) => {
+      const endpoint = x.OutputValue;
+      functionObject.events.forEach((event) => {
+        if (event.http) {
+          let method;
+          let path;
+          if (typeof event.http === 'object') {
+            method = event.http.method.toUpperCase();
+            path = event.http.path;
+          } else {
+            [method, path] = event.http.split(' ');
+            method = method.toUpperCase();
+          }
+          this.serverless.cli.consoleLog(`  ${method} - ${endpoint}/${path}`);
+        }
+      });
+    });
+}
